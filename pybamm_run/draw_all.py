081759e66ebabe006c81a0f9b3042ff58bfd2ce6
Archie Yao <461731182@qq.com>
2023-04-16 23:10:46 -0400

    update figure size & captions

--- pybamm_run/draw_all.py
+++ pybamm_run/draw_all.py
@@ -7,7 +7,7 @@ mpl.rc('font',family='Arial')
 
 
 # no error plot
-fig, ax = plt.subplots(2, 2, figsize=(12, 10))
+fig, ax = plt.subplots(2, 2, figsize=(12, 11))
 c_rates = [0.05, 0.5, 1.0, 2.0] 
 c_rates_label = ["C/20", "C/2", "1C", "2C"]
 filenames = ["LFP_25degC_Co20.csv", "LFP_25degC_Co2.csv", "LFP_25degC_1C.csv", "LFP_25degC_2C.csv"]
@@ -49,26 +49,20 @@ for i in range(0, len(c_rates)):
     ax_now.set_xlabel("Time (s)", fontsize=20)
     ax_now.set_ylabel("Voltage (V)", fontsize=20)
     ax_now.set_ylim([1.9, 3.8])
-<<<<<<< HEAD
     ax_now.set_xlim([-t.max()/100, t.max()*1.01])
     ax_now.legend(fontsize=16)
     ax_now.tick_params(axis='both', which='major', labelsize=20)  
-=======
-    ax_now.set_xlim([-t.max()/100, t.max()*1.02])
-    ax_now.legend(fontsize=12)
-    ax_now.tick_params(axis='both', which='major', labelsize=14)  
->>>>>>> 5d3b39aab1fb3595963b1b281513ad302898f1cf
 plt.savefig('Figure3.png', dpi=200, bbox_inches='tight') 
 plt.close()
 
 # with error plot
-fig, ax = plt.subplots(4, 2, figsize=(12, 16))
+fig, ax = plt.subplots(4, 2, figsize=(17, 20))
 c_rates = [0.05, 0.5, 1.0, 2.0] 
 c_rates_label = ["C/20", "C/2", "1C", "2C"]
 filenames = ["LFP_25degC_Co20.csv", "LFP_25degC_Co2.csv", "LFP_25degC_1C.csv", "LFP_25degC_2C.csv"]
 rmses_diffthermo = []
 rmses_AboutEnergy = []
-for i in range(0, len(c_rates)):
+for i in range(0, len(c_rates)):   
     # load true data
     filename = filenames[i]
     data = pd.read_csv(
@@ -99,31 +93,32 @@ for i in range(0, len(c_rates)):
     ax_now = ax[i][0]
     ax_now.plot(voltage_data[:, 0], voltage_data[:, 1], "k-", label=f"Experiment, {c_rates_label[i]}")
     ax_now.plot(t, V, "b--", label=f"This Work, {c_rates_label[i]}")
-    ax_now.plot(t_a, V_a, "r--", label=f"AboutEnergy, {c_rates_label[i]}")
-    ax_now.set_xlabel("Time (s)", fontsize=14)
-    ax_now.set_ylabel("Voltage (V)", fontsize=14)
+    # ax_now.plot(t_a, V_a, "r--", label=f"AboutEnergy, {c_rates_label[i]}")
+    ax_now.set_xlabel("Time (s)", fontsize=20)
+    ax_now.set_ylabel("Voltage (V)", fontsize=20)
     ax_now.set_ylim([1.9, 3.8])
     ax_now.set_xlim([-t.max()/100, t.max()*1.01])
-    ax_now.legend(fontsize=12)
-    ax_now.tick_params(axis='both', which='major', labelsize=14)  
+    ax_now.legend(fontsize=16)
+    ax_now.tick_params(axis='both', which='major', labelsize=20)  
     # Error plot
     ax_now = ax[i][1]
     # this work
     rmse = np.sqrt( np.nanmean((voltage_data[:, 1] - V)**2) ) * 1000
-    label = f"This Work, {c_rates_label[i]}, RMSE = "
+    # label = f"This Work, {c_rates_label[i]}, RMSE = "
+    label = f"{c_rates_label[i]}, RMSE = "
     label = label + "%.3f mV" %(rmse)
     ax_now.plot(t, (V - voltage_data[:, 1]) * 1000, "b-", label = label)
-    # about energy
-    rmse = np.sqrt( np.nanmean((voltage_data[:, 1] - V_a)**2) ) * 1000
-    label = f"About Energy, {c_rates_label[i]}, RMSE = "
-    label = label + "%.3f mV" %(rmse)
-    ax_now.plot(t, (V_a - voltage_data[:, 1]) * 1000, "r-", label = label)
+    # # about energy
+    # rmse = np.sqrt( np.nanmean((voltage_data[:, 1] - V_a)**2) ) * 1000
+    # label = f"About Energy, {c_rates_label[i]}, RMSE = "
+    # label = label + "%.3f mV" %(rmse)
+    # ax_now.plot(t, (V_a - voltage_data[:, 1]) * 1000, "r-", label = label)
     # settings
-    ax_now.set_xlabel("Time (s)")
-    ax_now.set_ylabel("Error (mV)")
+    ax_now.set_xlabel("Time (s)", fontsize=20)
+    ax_now.set_ylabel("Error (mV)", fontsize=20)
     ax_now.set_xlim([-t.max()/100, t.max()*1.01])
-    ax_now.legend(fontsize=12)
-    ax_now.tick_params(axis='both', which='major', labelsize=14)  
+    ax_now.legend(fontsize=16)
+    ax_now.tick_params(axis='both', which='major', labelsize=20)  
 
 plt.savefig('Figure3_with_error.png', dpi=200, bbox_inches='tight') 
 plt.close()
