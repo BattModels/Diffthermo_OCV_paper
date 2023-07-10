73f3c51a0ea262685af7f7d546e7115206ae6921
yaomz16 <461731182@qq.com>
2023-07-10 17:06:29 -0400

    changes for viewpoint

--- LFP/draw_all.py
+++ LFP/draw_all.py
@@ -67,9 +67,9 @@ mpl.rc('font',family='Arial')
 plt.figure(figsize=(5.5,4))
 ax = plt.gca()
 plt.plot(SOC_true,OCV_true,'k-',label="True OCV", markersize=1.5)
-plt.plot(SOC_diffthermo,OCV_diffthermo,'r.',label="This work", markersize=4)
+plt.plot(SOC_diffthermo,OCV_diffthermo,'r-.',label="This work", markersize=4)
 # plt.plot(SOC_diffthermo_splines,OCV_diffthermo_splines,'c-.',label="Diffthermo w/ splines", markersize=1.5)
-plt.plot(SOC_julia,OCV_julia,'b>',label="Monotonic RK", markersize=1.5)
+plt.plot(SOC_julia,OCV_julia,'b->',label="Monotonic RK", markersize=1.5)
 plt.plot(SOC_RK,OCV_RK,'g-.',label="Regular RK", markersize=1.5)
 plt.plot(SOC_Plett,OCV_Plett,'m-',label="Skewed RK", markersize=1.5)
 plt.legend(fontsize=10, frameon=False)
@@ -85,6 +85,30 @@ for axis in ['top','bottom','left','right']:
 ax.tick_params(width=2)
 plt.savefig('all_fit.png', dpi=200, bbox_inches='tight') 
 plt.close()
+
+# error plot on log scale
+plt.figure(figsize=(5.5,4))
+ax = plt.gca()
+plt.plot(SOC_diffthermo,np.abs(OCV_diffthermo-OCV_true),'r-.',label="This work", markersize=4)
+# plt.plot(SOC_diffthermo_splines,OCV_diffthermo_splines,'c-.',label="Diffthermo w/ splines", markersize=1.5)
+plt.plot(SOC_julia,np.abs(OCV_diffthermo-OCV_julia),'b->',label="Monotonic RK", markersize=1.5)
+plt.plot(SOC_RK,np.abs(OCV_diffthermo-OCV_RK),'g-.',label="Regular RK", markersize=1.5)
+plt.plot(SOC_Plett,np.abs(OCV_diffthermo-OCV_Plett),'m-',label="Skewed RK", markersize=1.5)
+plt.legend(fontsize=10, frameon=False)
+plt.xlim([0.0,1.0])
+plt.yscale("log")
+plt.xlabel("SOC", fontsize=14)
+plt.xticks(fontsize=14)
+plt.ylabel("Error (V)", fontsize=14)
+plt.yticks(fontsize=14)
+# plt.tight_layout()
+for axis in ['top','bottom','left','right']:
+    ax.spines[axis].set_linewidth(2)
+ax.tick_params(width=2)
+plt.savefig('error_plot_on_log_scale.png', dpi=200, bbox_inches='tight') 
+plt.close()
+
+
 
 # figure zoomed-in for Regular RK
 plt.figure(figsize=(2, 1.5))
---
+++ violators/README.md
@@ -0,0 +1,23 @@
+# README
+
+Models that violates monotonicity condition (and thus the second law of thermo)
+
+
+
+RK_Plett_LFP.npz: skewed RK for LFP, from Plett's textbook, violating region x=[0.08, 0.16]
+
+RK_Plett_graphite.npz: skewed RK for MCMB, from Plett's textbook, violating region x=[0.20, 0.60]
+
+RK_Plett_LTO.npz: skewed RK for LTO, from Plett's textbook, violating region x=[0.00, 0.20]
+
+RK_Plett_LCO.npz: skewed RK for LCO, from Plett's textbook, violating region x=[0.00, 0.20]
+
+RK_Plett_LMO.npz: skewed RK for LMO, from Plett's textbook, violating region x=[0.04, 0.20]
+
+<!-- RK_Plett_NCA.npz: skewed RK for NCA, from Plett's textbook, violating region x=[0.10, 0.20]  # this fit is probably problematic itself, it's so off -->
+
+RK_Plett_NMC.npz: skewed RK for NMC, from Plett's textbook, violating region x=[0.04, 0.40] and 
+
+
+
+
