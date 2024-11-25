888e970a040e8077db4194aaa8e8002c1aa40cdf
yaomz16 <461731182@qq.com>
2024-11-25 14:15:09 -0500

    debugged LMP fitting main.py

--- data_for_manuscripts/Chebyshev_fit/LMP/main.py
+++ data_for_manuscripts/Chebyshev_fit/LMP/main.py
@@ -12,11 +12,11 @@ df.to_csv("LiMnPO4_soc.csv", index=False, header=False)
 params_list = train(datafile_name='LiMnPO4_soc.csv', 
                         number_of_Omegas=5, 
                         polynomial_style = "Chebyshev",
-                        learning_rate = 2000.0, 
-                        total_training_epochs = 1000,
+                        learning_rate = 1000.0, 
+                        total_training_epochs = 10000,
                         loss_threshold = 0.01,
-                        G0_rand_range=[-10*5000,-5*5000], 
-                        Omegas_rand_range=[-10*100,10*100],
+                        G0_rand_range=[-100*5000,-50*5000], 
+                        Omegas_rand_range=[-100*100,100*100],
                         records_y_lims = [2.5,4.75])
 write_ocv_functions(params_list, polynomial_style = "Chebyshev")
 
